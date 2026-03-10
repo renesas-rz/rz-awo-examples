ab9029a4685a36bc4e1e57da003932499aab68d5
Khanh Tran <khanh.tran.te@renesas.com>
2026-03-10 12:59:44 +0700

    Update Example projects with RZ FSP v4.0.0

--- evk_rzg3s/freertos_w_awo_rzg3s_evk_example/script/postbuild.sh
+++ evk_rzg3s/freertos_w_awo_rzg3s_evk_example/script/postbuild.sh
@@ -11,8 +11,8 @@ filebody=${inputfilename%.*}
 
 arm-none-eabi-objcopy -O binary -j .text.secure_vector ${inputfilename} ${filebody}_secure_vector.bin
 arm-none-eabi-objcopy -O binary -j .text.non_secure_vector ${inputfilename} ${filebody}_non_secure_vector.bin
-arm-none-eabi-objcopy -O binary -j .text.secure_code ${inputfilename} ${filebody}_secure_code.bin
-arm-none-eabi-objcopy -O binary -j .text.non_secure_code -j .data ${inputfilename} ${filebody}_non_secure_code.bin
+arm-none-eabi-objcopy -O binary -j .text.secure_code -j .data ${inputfilename} ${filebody}_secure_code.bin
+arm-none-eabi-objcopy -O binary -j .text.non_secure_code ${inputfilename} ${filebody}_non_secure_code.bin
 
 arm-none-eabi-objcopy -O binary ${filebody}.elf -R .rodata.bootparam ${filebody}.bin
 arm-none-eabi-objcopy -I binary -O srec --srec-forceS3 --adjust-vma 0x80200000 ${filebody}.bin ${filebody}.srec
